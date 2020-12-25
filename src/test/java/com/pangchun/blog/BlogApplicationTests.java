3b35632af3d5f79de470c4165cf8c09854b49c49
pangchun <1007920179@qq.com>
2020-12-25 13:02:33 +0800

    提交信息：初始化项目

---
+++ src/test/java/com/pangchun/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangchun.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
