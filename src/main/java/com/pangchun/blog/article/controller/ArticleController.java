bb5a6a5209257a2676e903b39d49231707ed36f0
pangchun <pangchunun_mail@qq.com>
2021-01-05 22:25:49 +0800

    提交信息：完成文章保存功能，接下来做文章发布后在前端页面显示的功能

---
+++ src/main/java/com/pangchun/blog/article/controller/ArticleController.java
@@ -0,0 +1,42 @@
+package com.pangchun.blog.article.controller;
+
+import com.pangchun.blog.article.dto.ArticleDTO;
+import com.pangchun.blog.article.service.ArticleService;
+import com.pangchun.blog.entity.Article;
+import com.pangchun.blog.support.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+//TODO：文章发布可以通过添加文章到数据库，添加发布时间字段，通过取出发布时间最新的文章显示到前端页面即可;
+/**
+ * 文章API
+ *
+ * @author : pangchun
+ * @date : 2021-1-05 15:36
+ * @description : 文章API
+ * @version : v1.0
+ */
+@Api(tags = "文章API")
+@Controller
+@RequestMapping("/article")
+@Slf4j
+public class ArticleController {
+
+    @Resource
+    ArticleService articleService;
+
+    @ApiOperation("文章-发布")
+    @PostMapping("/publish")
+    @ResponseBody
+    public ResponseResult<Article> publish(@RequestBody ArticleDTO articleDTO) {
+
+        log.info(articleDTO.toString());
+
+        return articleService.publish(articleDTO);
+    }
+}
---
+++ src/main/java/com/pangchun/blog/entity/Article.java
@@ -0,0 +1,46 @@
+package com.pangchun.blog.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+//TODO: 继承文章状态，完成保存草稿功能；
+
+/**
+ * 文章实体类
+ *
+ * @author : pangchun
+ * @date : 2021-1-5 15:27
+ * @description : 文章实体类
+ * @version : v1.0
+ */
+@ApiModel("文章实体")
+@Data
+@Entity
+public class Article {
+
+    /** 主键编号 */
+    @ApiModelProperty("主键id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer uid;
+
+    /** 文章标题 */
+    @ApiModelProperty("文章标题")
+    private String title;
+
+    /** 文章描述 */
+    @ApiModelProperty("文章描述")
+    private String description;
+
+    /** 文章内容 */
+    @ApiModelProperty("文章内容")
+    private String content;
+
+}
